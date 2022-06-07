b9b24711bc947209aec233e9fcb83e8b740b0569
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-07 13:03:41 +0700

    async sign jwt token

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.post('/api/posts',(req,res)=>{
     });
 });
 
+app.post('/api/login',(req,res)=>{
+    // authenticate user and get user 
+    let user ={
+        id:1,
+        name : 'Hlaing Min Than',
+        'email':'hlaingminthan92@gmail.com'
+    };
+
+    jwt.sign({user},'secretkey',(err,token)=>{
+        res.json({
+            token
+        });
+    })
+});
+
 app.listen('3000',()=>{
     console.log('app is running on 3000 port');
 });
